84e5fd45f24473ccbe18e6a7608cb55f52d805a8
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-04-09 08:51:40 +0545

    web form working

---
+++ userRegistration/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="userRegistration.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label id="l1" Text ="Username" runat = "server"></asp:Label>
+            <asp:TextBox ID="uname" runat = "server"></asp:TextBox><br />
+
+            <asp:Label ID="l2" Text ="Password" runat = "server"></asp:Label>
+            <asp:TextBox ID="pass"  TextMode="Password" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="l3" Text ="RePassword" runat = "server"></asp:Label>
+            <asp:TextBox ID="repass"  TextMode="Password" runat="server"></asp:TextBox><br />
+
+            <asp:Label ID="l4" Text ="Gender" runat = "server"></asp:Label>
+            <asp:RadioButton ID="r1" Text="male" GroupName="gender" value="male" runat="server"></asp:RadioButton><br />
+            <asp:RadioButton ID="r2" Text="female" GroupName="gender" value="female" runat="server"></asp:RadioButton><br />
+
+            <asp:Label ID="l5" Text ="Course" runat = "server"></asp:Label>
+            <asp:CheckBox ID="c1" Text="C" value="C" runat="server" /><br />
+            <asp:CheckBox ID="c2" Text="C++" value="C++" runat="server" /><br />
+            <asp:CheckBox ID="c3" Text="java" value="java" runat="server" /><br />
+
+            <asp:Label ID="l6" Text ="Country" runat = "server"></asp:Label>
+            <asp:DropDownList ID="country" runat="server">
+              <asp:ListItem Value="">Select your country</asp:ListItem>
+              <asp:ListItem>Nepal</asp:ListItem>
+              <asp:ListItem>China</asp:ListItem>
+              <asp:ListItem>India</asp:ListItem>
+              <asp:ListItem>Other</asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:Button ID="submit" Text="submit" OnClick="btn_submit" runat="server"></asp:Button>
+            <asp:Label ID="l7" Text ="Result" runat = "server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ userRegistration/WebForm1.aspx.cs
@@ -0,0 +1,97 @@
+﻿using Antlr.Runtime.Tree;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace userRegistration
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btn_submit(object sender, EventArgs e)
+        {
+            //extracting for feilds
+            string username = uname.Text;
+            string password = pass.Text;
+            string repassword = repass.Text;
+         
+            string gender;
+            if (r1.Checked)
+            {
+                gender = r1.Text;
+            }
+            else if (r2.Checked)
+            {
+                gender = r2.Text;
+            }
+            else
+            {
+                l7.Text = "Please select a gender";
+                return;
+            }
+
+            string courses = "";
+            if (c1.Checked)
+            {
+                courses += c1.Text + ", ";
+            }
+            if (c2.Checked)
+            {
+                courses += c2.Text + ", ";
+            }
+            if (c3.Checked)
+            {
+                courses += c3.Text + ", ";
+            }
+
+            string coun = "";
+            if (country.SelectedValue == "")
+            {
+                coun = "enter your country";
+            }
+            else
+            {
+                coun = country.SelectedValue;
+            }
+
+            string cs = "Data Source=SHRIJALA-PC\\SQLEXPRESS;Initial Catalog=db_prime;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(cs);
+
+            try
+            {
+                conn.Open();
+                string insQuery = "insert into tbl_reg (username, password, gender, courses, country) VALUES (" +
+                    "@username, @password, @gender, @courses, @country)";
+          
+                SqlCommand sc = new SqlCommand(insQuery, conn);
+                sc.Parameters.AddWithValue("@username", username);
+                sc.Parameters.AddWithValue("@password", password);
+                sc.Parameters.AddWithValue("@repassword", repassword);
+                sc.Parameters.AddWithValue("@gender", gender);
+                sc.Parameters.AddWithValue("@courses", courses);
+                sc.Parameters.AddWithValue("@country", coun);
+
+                int res = sc.ExecuteNonQuery();
+                if (res > 0)
+                {
+                    l7.Text = "data inserted";
+                }
+                else
+                {
+                    l7.Text = "data not inserted";
+                }
+            }
+            catch (SqlException error)
+            {
+                l7.Text = error.Message;
+            }
+        }
+    }
+}
