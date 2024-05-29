765d784b6fdf5df16846397d1333526f080716a7
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-05-29 09:52:09 +0545

    added exam questions

---
+++ ExamQuestions/Constructor_Indexers_Encapsulation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamQuestions
+{
+    class Customer
+    {
+        private string name;
+
+        //properties
+        public int CustomerID { get; set; }
+        //this will encapsulate the private field name
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Indexers
+        private string[] purchasedItem = new string[10];
+        public string this[int i]
+        {
+            get { return purchasedItem[i]; }
+            set { purchasedItem[i] = value; }
+        }
+
+        public int Length { 
+            get { return purchasedItem.Length; }
+        }
+
+        //Constructor
+        public Customer(int ID, string name)
+        {
+            Name = name;
+            CustomerID = ID;
+        }
+    }
+}
---
+++ ExamQuestions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamQuestions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer(1, "bruh");
+
+            //adding purchased item to demonstrate the indexer
+            //for(int i = 0; i<10; i++)
+            //{
+                //c[i] = "Item " + i;
+            //}
+
+            Console.WriteLine("The customer name is: " + c.Name + " And ID is: " + c.CustomerID);
+            Console.WriteLine(c.Name + " has purchased " + c.Length + " items");
+            for(int j = 0; j<c.Length; j++)
+            {
+                Console.WriteLine(c[j]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
