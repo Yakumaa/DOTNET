38d5c9174febbf866633d95fb5ca61bc1fb5c3f0
Shrish Maharjan <maharjanshrish8@gmail.com>
2024-07-11 00:39:14 +0545

    removed ignored files

---
+++ firstTry/MultiLevelInheritance.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace firstTry
+{
+    public class TwoWheeler
+    {
+        private int reg;
+        private string name;
+        private string brand;
+
+        public int Reg
+        {
+            get { return reg; }
+            set { reg = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+    }
+
+    class Bike : TwoWheeler
+    {
+        private string cc;
+        private string fuelCapacity;
+
+        public string Cc
+        {
+            get { return cc; }
+            set { cc = value; }
+        }
+
+        public string FuelCapacity
+        {
+            get { return fuelCapacity; }
+            set { fuelCapacity = value; }
+        }
+    }
+
+    class ElectricBike : Bike
+    {
+        private string range;
+        public string Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+    }
+}
+
---
+++ firstTry/lambdaExpresionDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+//lambda expression is like a nonymouse function except that i doesnt have data type. it uses => symbol which has two part left side of
+//symbol is expression or statement 
+//symbol=>expression or statement
+
+namespace firstTry
+{
+    class lambdaExpresionDemo
+    {
+        public void setExpression()
+        {
+            List<int> num1 = new List<int>() { 30, 40, 50, 60, 12 };
+
+            //using lambda expression find out whether the number is even or not
+            var result = num1.Select(x => (x % 2) == 0);
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+            List<int> l2 = num1.FindAll(x => (x % 2) == 0);
+            foreach (var res1 in l2)
+            {
+                Console.WriteLine(res1);
+            }
+        }
+
+
+        //lambda expression in class
+        public class Employees
+        {
+            private int id;
+            private string name;
+            private string department;
+
+            public int Id { get { return id; } set { id = value; } }
+
+            public string Name { get { return name; } set { name = value; } }
+
+            public string Department { get { return department; } set { department = value; } }
+        }
+    }
+}
